593ac485045a1249cbbff931b125de88f768557b
eduardo <eduardocserra@outlook.com>
2018-09-11 11:13:56 -0300

    Adicionada capitalização de nomes de escola e categoria de curso antes de salvar

--- app/models/course_category.rb
+++ app/models/course_category.rb
@@ -1,4 +1,8 @@
 class CourseCategory < ApplicationRecord
   has_many :courses
   has_one_attached :logo
+  before_save :capitalize_name
+  def capitalize_name
+    self.name=self.name.split.map(&:capitalize).join(' ')
+  end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,8 @@
 class School < ApplicationRecord
   has_many :courses
   has_one_attached :logo
+  before_save :capitalize_name
+  def capitalize_name
+    self.name=self.name.split.map(&:capitalize).join(' ')
+  end
 end

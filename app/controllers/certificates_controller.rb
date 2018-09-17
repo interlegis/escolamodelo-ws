95532e72375374ba508f2a5450e030dadb1f7303
eduardo <eduardocserra@outlook.com>
2018-09-17 18:26:22 -0300

    Corrigido formato do issue_date do certificado

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -2,7 +2,8 @@ class CertificatesController < ApplicationController
   def adicionar_certificado
     school=School.find_by(initials: params[:school])
     course=Course.find_by('ead_id = ? and school_id = ?', params[:certificate][:course], school.id)
+    time=DateTime.strptime(params[:certificate][:date], '%s')
     user=User.find_by(cpf: params[:certificate][:course]) #Atualmente cpf tem q ter . e -
-    Certificate.new(course_id: course, user_id: user, issue_date: params[:certificate][:date], grade: params[:certificate][:grade], code_id: params[:certificate][:code])
+    Certificate.new(course_id: course, user_id: user, issue_date: time, grade: params[:certificate][:grade], code_id: params[:certificate][:code])
   end
 end
--- db/migrate/20180917130946_create_certificates.rb
+++ db/migrate/20180917130946_create_certificates.rb
@@ -3,7 +3,7 @@ class CreateCertificates < ActiveRecord::Migration[5.2]
     create_table :certificates do |t|
       t.references :course, foreign_key: true
       t.references :user, foreign_key: true
-      t.date :issue_date
+      t.timestamp :issue_date
       t.float :grade
       t.string :code_id
 

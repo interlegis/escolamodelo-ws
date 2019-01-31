e373a38a491fcb652e6d02d941e64ef853ef7252
Matheus456 <matheus456456@gmail.com>
2019-01-31 15:49:36 -0200

    União dos métodos adicionar e atualizar escola em registrar escola

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,46 +1,32 @@
 class SchoolsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def adicionar_escola
+  def registrar_escola
     @api_key = ApiAccess.find_by(key: params[:key])
     if @api_key.present?
       if @api_key.api_access_level_id == 4
-        @school = School.new(school_params)
-        if @school.save
-          #@course.logo.attach(io: StringIO.new('https://saberes.senado.leg.br/images/logo_saberes_xl.png'), filename: 'logo_saberes.png', content_type: 'image/png')
+        if params[:initials_school].present?
+          @school = School.find_by(initials: params[:initials_school])
+          if !params[:school][:initials].present?
+            params[:school][:initials] = @school.initials
+          end
+          @school.update(school_params)
           render status: 200, json: {
-              message: "Escola criada com sucesso",
+              message: "Escola atualizada com sucesso",
           }.to_json
         else
-          render status: 400, json: {
-              message: "Não foi possível criar escola",
-          }.to_json
+          @school = School.new(school_params)
+          if @school.save
+            #@course.logo.attach(io: StringIO.new('https://saberes.senado.leg.br/images/logo_saberes_xl.png'), filename: 'logo_saberes.png', content_type: 'image/png')
+            render status: 200, json: {
+                message: "Escola criada com sucesso",
+            }.to_json
+          else
+            render status: 400, json: {
+                message: "Não foi possível criar escola",
+            }.to_json
+          end
         end
-      else
-        render status: 400, json: {
-            message: "Permissão negada",
-        }.to_json
-      end
-    else
-      render status: 400, json: {
-          message: "Chave não encontrada",
-      }.to_json
-    end
-  end
-
-  def atualizar_escola
-    @api_key = ApiAccess.find_by(key: params[:key])
-    if @api_key.present?
-      if @api_key.api_access_level_id == 4
-        @school = School.find_by(initials: params[:initials_school])
-        if !params[:school][:initials].present?
-          params[:school][:initials] = @school.initials
-        end
-        @school.update(school_params)
-        #verificar presença de imagem
-        render status: 200, json: {
-            message: "Escola atualizada com sucesso",
-        }.to_json
       else
         render status: 400, json: {
             message: "Permissão negada",
